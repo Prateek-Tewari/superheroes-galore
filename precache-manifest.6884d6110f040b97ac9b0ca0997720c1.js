42be8c7fdde9c9864e9f757ba539dd3ba9043e7f
Prateek_Tewari <hindustani1000@gmail.com>
2019-09-21 00:24:50 +0530

    Updates

---
+++ precache-manifest.6884d6110f040b97ac9b0ca0997720c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6292ba5f1639bb160606b015ea0758",
+    "url": "/superheroes-galore/index.html"
+  },
+  {
+    "revision": "61ea8838be3d914b36b3",
+    "url": "/superheroes-galore/static/css/main.39f1a7e8.chunk.css"
+  },
+  {
+    "revision": "5d4015b29460bbfc1efd",
+    "url": "/superheroes-galore/static/js/2.bdbe9fb0.chunk.js"
+  },
+  {
+    "revision": "61ea8838be3d914b36b3",
+    "url": "/superheroes-galore/static/js/main.17c1193e.chunk.js"
+  },
+  {
+    "revision": "5a9d9eff9b30745dc252",
+    "url": "/superheroes-galore/static/js/runtime~main.27e22b65.js"
+  },
+  {
+    "revision": "1d479916c9b3734b81c081ceef0cc428",
+    "url": "/superheroes-galore/static/media/cap.1d479916.jpg"
+  },
+  {
+    "revision": "5ca5b8ccbc378747ffefae889f8b2291",
+    "url": "/superheroes-galore/static/media/hulk.5ca5b8cc.jpeg"
+  },
+  {
+    "revision": "b9138c430c4b4dbf33eb0a2e8b108e0f",
+    "url": "/superheroes-galore/static/media/ironman.b9138c43.jpg"
+  },
+  {
+    "revision": "afa5ba488eaf229077a0784db8fb2f85",
+    "url": "/superheroes-galore/static/media/thor.afa5ba48.jpg"
+  },
+  {
+    "revision": "0c31e5a4215c061d8309e62b68dda9c1",
+    "url": "/superheroes-galore/static/media/widow.0c31e5a4.jpg"
+  }
+]);
